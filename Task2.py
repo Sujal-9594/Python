bd2a407169ee5cb17024ff812ae35e2738a6d8d3
Sujal-9594 <sujal.github9594@gmail.com>
2025-06-02 02:11:38 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,7 @@
+#reate a Personalized Greeting
+
+first_name=input('Enter your first name: ')
+second_name=input('Enter your last name: ')
+
+full_name=first_name + second_name
+print('Hello, '+full_name+'! Welcome to the python program')
