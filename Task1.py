a1fa9ee518b63a00d8bd52fab8c36d19eead9501
Sujal-9594 <sujal.github9594@gmail.com>
2025-06-02 01:35:22 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,23 @@
+#Perform Basic Mathematical Operations
+
+n1=input('Enter first number: ')
+n2=input('Enter second number: ')
+
+n1=int(n1)
+n2=int(n2)
+
+add=n1+n2
+print('Addition: ',add)
+
+sub=n1-n2
+print('Subtraction: ',sub)
+
+mul=n1*n2
+print('Multiplication: ',mul)
+
+div=n1/n2
+print('Division: ',div)
+
+
+
+
