67608dc0b0059ba6688837d00fa781847b152c86
Sujal-9594 <sujal.github9594@gmail.com>
2025-07-10 14:59:58 +0530

    Add files via upload
    Task 2: Using the Math Module for Calculations

---
+++ Assignment 3 Task 2.py
@@ -0,0 +1,11 @@
+from math import *
+num = float(input("Enter a number: "))
+
+square_root = sqrt(num)
+log_value = log(num)
+sin_value = sin(num)
+
+print('Square root:',square_root)
+print('Logarithm:',log_value)
+print('Sine:',sin_value)
+
